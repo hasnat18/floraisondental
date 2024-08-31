9cc3ac1cc75a9af5f18ff5c8bfce80adbf220a22
hasnat18 <hsntkhan1614@gmail.com>
2024-09-01 03:38:31 +0500

    new

--- admin/config.php
+++ admin/config.php
@@ -3,11 +3,12 @@
 
 return [
     'mail' => [
-        'host' => 'smtp.stackmail.com',
+        'host' => 'smtp.gmail.com',
         'smtp_auth' => true,
-        'port' => 465,
-        'username' => 'floraisondental@alruya.link', // Correct username
-        'password' => 'jzn7pcz01o', // Replace with the actual email account password
+        'port' => 465, // You can also use 587 for TLS
+        'username' => 'Floraisondentalinfo@gmail.com', // Your Gmail address
+        'password' => 'utzq rvhv ekzt yxji', // Replace with the actual app-specific password if using 2FA
+        'encryption' => 'ssl', // Use 'tls' if port is 587
     ],
 ];
 ?>
