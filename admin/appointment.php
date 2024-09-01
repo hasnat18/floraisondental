a85893cc9ec4a5739c56651228280622954c5b2e
hasnat18 <hsntkhan1614@gmail.com>
2024-09-02 04:41:57 +0500

    new

--- admin/appointment.php
+++ admin/appointment.php
@@ -21,9 +21,10 @@ $response = array();
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    header('Access-Control-Allow-Origin: https://v2.floraisondental.com');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
-    http_response_code(200); // Send OK status
+    header('Access-Control-Max-Age: 86400'); // Cache for 1 day
     exit(0);
 }
 
